ac9fded965f4bbe4efc1d2fc523b65e7c322d298
Nathan Douglas <github@darkdell.net>
2024-02-01 21:21:35 -0500

    Massive simplification.

--- goldentooth.sh
+++ goldentooth.sh
@@ -2,6 +2,23 @@
 
 ansible_path="${HELLHOLT_ANSIBLE_PATH:-${HOME}/Projects/cluster}";
 
+# Install Ansible dependencies.
+function goldentooth:install() {
+  pushd "${ansible_path}" > /dev/null;
+  ansible-galaxy install -r requirements.yml;
+  pushd kubespray > /dev/null;
+  pip3 install -r requirements.txt;
+  popd > /dev/null;
+  popd > /dev/null;
+}
+
+# Lint all roles.
+function goldentooth:lint() {
+  pushd "${ansible_path}" > /dev/null;
+  ansible-lint;
+  popd > /dev/null;
+}
+
 # Edit the vault.
 function goldentooth:edit_vault() {
   pushd "${ansible_path}" > /dev/null;
@@ -18,207 +35,34 @@ function goldentooth:ansible_playbook() {
   popd > /dev/null;
 }
 
-# Run a specified Ansible role.
-function goldentooth:ansible_role() {
-  : "${2?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP> <ROLE> <TASKFILE>"}";
-  local host_expression="${1}";
-  local role_name="${2}";
-  pushd "${ansible_path}" > /dev/null;
-  ansible-playbook ${@:3} /dev/stdin <<END
----
-- hosts: $host_expression
-  roles:
-    - '$role_name'
-END
-  popd > /dev/null;
-}
-
-# Run a specified Ansible task.
-function goldentooth:ansible_task() {
-  : "${3?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP> <ROLE> <TASKFILE>"}";
-  local host_expression="${1}";
-  local role_name="${2}";
-  local task_file="${3}";
-  pushd "${ansible_path}" > /dev/null;
-  ansible-playbook ${@:4} /dev/stdin <<END
----
-- hosts: $host_expression
-  remote_user: 'root'
-  tasks:
-
-  - name: 'Execute $role_name:$task_file'
-    ansible.builtin.include_role:
-      name: '$role_name'
-      tasks_from: '$task_file'
-END
-  popd > /dev/null;
-}
-
-# Install Ansible dependencies.
-function goldentooth:install() {
-  pushd "${ansible_path}" > /dev/null;
-  ansible-galaxy install -r requirements.yml;
-  popd > /dev/null;
-}
-
-# Lint all roles.
-function goldentooth:lint() {
-  pushd "${ansible_path}" > /dev/null;
-  ansible-lint;
-  popd > /dev/null;
-}
-
-# Configure Raspberry Pis.
-function goldentooth:configure() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.configure' "${args}";
-  popd > /dev/null;
-}
-
-# Set the Bash Prompt on a specified node or group.
-function goldentooth:set_bash_prompt() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.set_bash_prompt' "${args}";
-  popd > /dev/null;
-}
-
-# Set the hostname on a specified node or group.
-function goldentooth:set_hostname() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.set_hostname' "${args}";
-  popd > /dev/null;
-}
-
-# Set the MotD on a specified node or group.
-function goldentooth:set_motd() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.set_motd' "${args}";
-  popd > /dev/null;
-}
-
-# Setup security settings.
-function goldentooth:setup_security() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.setup_security' "${args}";
-  popd > /dev/null;
-}
-
-# Setup the load balancer.
-function goldentooth:setup_load_balancer() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.setup_load_balancer' "${args}";
-  popd > /dev/null;
-}
-
-# Setup the Kubernetes repository.
-function goldentooth:setup_k8s_repo() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.setup_k8s_repo' "${args}";
-  popd > /dev/null;
-}
-
-# Setup the Kubernetes control plane.
-function goldentooth:setup_k8s_cp() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.setup_k8s_cp' "${args}";
-  popd > /dev/null;
-}
-
-# Create the cluster.
-function goldentooth:create_cluster() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.create_cluster' "${args}";
-  popd > /dev/null;
-}
-
-# Prepare the cluster but don't actually setup Kubernetes.
-function goldentooth:prepare_cluster() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.prepare_cluster' "${args}";
-  popd > /dev/null;
-}
-
-# Reset the cluster.
-function goldentooth:reset_cluster() {
-  : "${1?"Usage: ${FUNCNAME[0]} <HOSTNAME|GROUP>"}";
-  local host_expression="${1}";
-  local args="${@:2}";
-  pushd "${ansible_path}" > /dev/null;
-  goldentooth:ansible_role "${host_expression}" 'goldentooth.reset_cluster' "${args}";
-  popd > /dev/null;
-}
-
-# Define associative array for subcommands and their descriptions.
-#
-# Associative arrays are a feature of Bash 4.0. If you're using a Mac, you'll
-# need to install Bash 4.0+ with Homebrew.
-declare -A subcommands=(
-  [usage]='Show usage information.'
-  [install]='Install dependencies.'
-  [lint]='Lint all roles.'
-  [ansible_task]='Run a specified Ansible task.'
-  [edit_vault]='Edit the vault.'
-  [autocomplete]='Output autocomplete information.'
-  [configure]='Configure the hosts (via e.g. `raspi-config`, `config.txt`).'
-  [set_bash_prompt]='Set Bash prompt.'
-  [set_hostname]='Set hostname.'
-  [set_motd]='Set MotD.'
-  [setup_security]='Apply some security settings.'
-  [setup_k8s_repo]='Setup the Kubernetes repository.'
-  [setup_k8s_cp]='Setup the Kubernetes control plane.'
-  [setup_load_balancer]='Setup the load balancer.'
-  [prepare_cluster]='Setup everything but Kubernetes.'
-  [create_cluster]='Create the cluster.'
-  [reset_cluster]='Reset the cluster.'
-)
-
-# Show usage information.
+# Display usage information.
 function goldentooth:usage() {
-  local subcommand_width='24';
-  local subcommand_column="%${subcommand_width}s    %s\n";
   echo 'Usage: goldentooth <subcommand> [arguments...]';
   echo '';
-  echo 'Subcommands: ';
-  for cmd in "${!subcommands[@]}"; do
-    printf "$subcommand_column" "$cmd" "${subcommands[$cmd]}"
-  done
+  echo 'Subcommands:';
+  pushd "${ansible_path}" > /dev/null;
+  for playbook in playbooks/*.yaml; do
+    playbook_name="$(basename "${playbook}" '.yaml')";
+    first_line="$(head -n 1 "${playbook}")";
+    description="$(echo "${first_line}" | sed -n 's/^# Description: \(.*\)/\1/p')";
+    if [[ -n $description ]]; then
+      printf "%24s %s\n" "${playbook_name}" "${description}";
+    fi
+  done;
+  popd > /dev/null;
   echo '';
 }
 
 # Print autocomplete script.
 function goldentooth:autocomplete() {
-  local subcommands_string="${!subcommands[@]}"
-  echo "complete -W '$subcommands_string' goldentooth"
+  local subcommands_string="";
+  pushd "${ansible_path}" > /dev/null;
+  for playbook in playbooks/*.yaml; do
+    playbook_name="$(basename "${playbook}" .yaml)";
+    subcommands_string="${subcommands_string} ${playbook_name}";
+  done;
+  popd > /dev/null;
+  echo "complete -W '$subcommands_string' goldentooth";
 }
 
 # Primary function.
@@ -226,13 +70,12 @@ function goldentooth() {
   local subcommand="${1:-usage}";
   export OBJC_DISABLE_INITIALIZE_FORK_SAFETY='YES';
   export K8S_AUTH_KUBECONFIG='~/.kube/config';
-  shift;
   if type "goldentooth:${subcommand%:*}" > /dev/null 2>&1; then
     host_expression="${1:-all}";
     shift;
     "goldentooth:${subcommand%:*}" "${host_expression}" "${@:1}";
   else
-    goldentooth:usage;
+    goldentooth:ansible_playbook "${subcommand}" "${@:1}";
   fi;
 }
 
